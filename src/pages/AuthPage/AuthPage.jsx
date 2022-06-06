4f71761e52f64c9aa459ab5812b2075dba073fe6
nisha09 <nisha1947@gmail.com>
2022-06-06 16:32:48 -0600

    add styling to auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react';
+import './AuthPage.css';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
 
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
-    <main>
-      <h1>AuthPage</h1>
-      <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
-      { showSignUp ?
+    <div className='Auth-div'>
+      {/* <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button> */}
+      {/* { showSignUp ? */}
           <SignUpForm setUser={setUser} />
-          :
+          {/* : */}
+          <div class="vl"></div>
           <LoginForm setUser={setUser} />
-      }
-    </main>
+      {/* } */}
+    </div>
   );
 }
