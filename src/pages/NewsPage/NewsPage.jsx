c096d1b8f935a7ce061be39dac10b407aeb04b7a
nisha09 <nisha1947@gmail.com>
2022-06-08 09:17:34 -0600

    conditional rendering for api fetch call

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -4,26 +4,28 @@ import { useState, useEffect } from 'react';
 import * as newsAPI from '../../utilities/news-api';
 
 export default function NewsPage({ setUser }) {
-  // const [showSignUp, setShowSignUp] = useState(false);
-  const [news, setNews] = useState({});
-  useEffect(function() {
-    async function getNews() {
-      const news = await newsAPI.getAll();
-      setNews(JSON.parse(news));
-    }
-    getNews();
-  }, []);
+    const [isBusy, setBusy] = useState(true)
+    const [news, setNews] = useState({});
 
-  return (
-    <>
-    <h2> <h1>🗺</h1> News OnDemand </h2>
-    {
-        news.articles.map(function (d) {
-          return <li>{d.title}</li>;
-        })
-    }
-    </>
-  );
+    useEffect(function () {
+        async function getNews() {
+            const news = await newsAPI.getAll();
+            setBusy(false);
+            setNews((JSON.parse(news)));
+        }
+        getNews();
+    }, []);
+
+    return (
+        <>
+            <h2> <h1>🗺</h1> News OnDemand </h2>
+            {isBusy ? (<p></p>) : (
+                news && news.articles.map(function (d, id) {
+                    return <li>{d.title}</li>;
+                })
+            )}
+        </>
+    );
 }
 
 
