2df93a95bc5fe1d9af771abdf11b2fe1d4627427
nisha09 <nisha1947@gmail.com>
2022-06-09 15:38:03 -0600

    add graph

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,21 +1,41 @@
 import '../NewsPage/NewsPage.css';
 import { useState, useEffect } from 'react';
 import * as newsAPI from '../../utilities/news-api';
+import BarChart from "../../components/BarChart/BarChart";
+
 
 export default function NewsPage({ setUser }) {
+    
     const [isBusy, setBusy] = useState(true)
     const [news, setNews] = useState([]);
-     let width = '500';
-     let color = getRandomRGBA();
+    const [userData, setUserData] = useState({});
 
     useEffect(function () {
         async function getNews() {
             const trends = await newsAPI.getAll();
-            const news = trends.filter(function(n){
+            const news = trends.filter(function (n) {
                 return n.tweet_volume !== null
             })
             setBusy(false);
             setNews(news);
+            setUserData({
+                labels: news.map((data) => data.name),
+                datasets: [
+                  {
+                    label: "What's Trending Today",
+                    data: news.map((data) => data.tweet_volume),
+                    backgroundColor: [
+                      "rgba(75,192,192,1)",
+                      "#ecf0f1",
+                      "#50AF95",
+                      "#f3ba2f",
+                      "#2a71d0",
+                    ],
+                    borderColor: "black",
+                    borderWidth: 2,
+                  },
+                ],
+              })
         }
         getNews();
         console.log(news);
@@ -23,28 +43,12 @@ export default function NewsPage({ setUser }) {
 
     return (
         <>
-            <h2> <h1>🗺</h1> What's Trending </h2>
-            <div className='chart-div'>
-            {isBusy ? (<p></p>) : (
-               
-               news.map(function (d, id) {
-                {width = width - 10}
-                {color = getRandomRGBA()}
-                    return (
-                        <div style={{ opacity: '0.8', width:'30px' , height: `${width}px`, background:`${color}`, border:'1px solid black'}}>{d.name} {d.tweet_volume}</div>
-                    ) 
-                })
-            )}
-            </div>
+           {(isBusy ? <p/> : <div className="App">
+                <div style={{ width: 700 }}>
+                    <BarChart chartData={userData} />
+                </div>
+            </div>)}
         </>
 
     );
 }
-
-function getRandomRGBA() {
-    return `rgba(${randomBetween(0, 255)}, ${randomBetween(0, 255)}, ${randomBetween(0, 255)}, ${randomBetween(2, 10) / 10})`;
-  }
-
-  function randomBetween(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
